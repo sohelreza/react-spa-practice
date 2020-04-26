7d7d155f62f50ba333aafadac8d4a480b0dfc027
sohelreza <61862128+sohelreza@users.noreply.github.com>
2020-04-27 01:26:01 +0600

    Add files via upload

---
+++ build/precache-manifest.b2c948ff38ec6034f81423f0cda0023b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb2db15d389b1b567f95c189b9c2c85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1be9bccc506b4647e46e",
+    "url": "/static/css/2.cd85e1b1.chunk.css"
+  },
+  {
+    "revision": "80e6ad69a87c32374442",
+    "url": "/static/css/main.1240b7ea.chunk.css"
+  },
+  {
+    "revision": "1be9bccc506b4647e46e",
+    "url": "/static/js/2.69c8ae37.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.69c8ae37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80e6ad69a87c32374442",
+    "url": "/static/js/main.6a07e3ac.chunk.js"
+  },
+  {
+    "revision": "2977a07cd6ef0e1a990d",
+    "url": "/static/js/runtime-main.6afc4dd5.js"
+  },
+  {
+    "revision": "a5df93c88656d9710697c158335316aa",
+    "url": "/static/media/computers3.a5df93c8.png"
+  },
+  {
+    "revision": "8068dc7df6ddc47df26ed67d3bb4fb31",
+    "url": "/static/media/dell.8068dc7d.jpg"
+  }
+]);
